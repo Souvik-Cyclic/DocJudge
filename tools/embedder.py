5de0b1e821de04779598227fb53b0c705cca36ff
Purab Ray <purabray2@gmail.com>
2026-06-19 13:56:59 +0530

    feat(tools): add _model()

--- tools/embedder.py
+++ tools/embedder.py
@@ -2,3 +2,11 @@
 from __future__ import annotations
 
 from functools import lru_cache
+
+from config import config
+
+@lru_cache(maxsize=1)
+def _model():
+    from sentence_transformers import SentenceTransformer
+
+    return SentenceTransformer(config.EMBED_MODEL)
